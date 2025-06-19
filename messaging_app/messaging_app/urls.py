0a838536bef2b14927b8988e234cdcf432d20aeb
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-06-20 00:00:10 +0300

    kk

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -35,6 +35,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('api/', include('chats.urls')),  # this enables /api/chat/messages/
+    path ('api/', include('chats.urls')),  # this enables /api/chat/message
     path('api-auth/', include('rest_framework.urls')), 
 ]
